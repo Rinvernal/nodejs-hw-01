af05e113c4c64c72673cd0769c2e769796ba57f0
Rinvernal <irina.elkina2006@gmail.com>
2025-01-15 23:14:10 +0200

    done step 2

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
-
-export const writeContacts = async (updatedContacts) => {};
+import fs from "node:fs/promises";
+import { createFakeContact } from './createFakeContact.js';
+  export const writeContacts = async (updatedContacts) => {
+    try {
+      const data = JSON.stringify(createFakeContact(), null, 2);
+      await fs.writeFile(PATH_DB, data, "utf8");
+      console.log("all right");
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+  writeContacts();
